99fe76eff2b0a55fc00816d92670a13e7a391a16
davidweichen <david19990212@gmail.com>
2023-09-25 23:50:39 -0700

    implement

--- dgemv-openmp.cpp
+++ dgemv-openmp.cpp
@@ -20,6 +20,18 @@ void my_dgemv(int n, double* A, double* x, double* y) {
       int thread_id = omp_get_thread_num();
       printf("my_dgemv(): Hello world: thread %d of %d checking in. \n", thread_id, nthreads);
       printf("my_dgemv(): For actual timing runs, please comment out these printf() and omp_get_*() statements. \n");
+      
+      double sum = 0.0;
+      //divide the work among the threads
+      for(int i=0;i<n;i++){
+         for(int j=0;j<n;j+=nthreads){
+            sum += A[i*n+j]*x[j];
+         }
+         #pragma omp critical
+         {
+            y[i] += sum;
+         }
+      }
    }
 
    // insert your dgemv code here. you may need to create additional parallel regions,
