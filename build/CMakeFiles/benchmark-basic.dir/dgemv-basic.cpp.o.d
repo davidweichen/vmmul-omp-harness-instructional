507e94a121a3ffe7b611ce35cdd57fc613a96c03
davidweichen <david19990212@gmail.com>
2023-10-02 21:03:16 -0700

    test

--- build/CMakeFiles/benchmark-basic.dir/dgemv-basic.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/benchmark-basic.dir/dgemv-basic.cpp.o: \
- /home/gator/vmmul-omp-harness-instructional/dgemv-basic.cpp \
- /usr/include/stdc-predef.h
--- build/CMakeFiles/benchmark-vectorized.dir/dgemv-vectorized.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/benchmark-vectorized.dir/dgemv-vectorized.cpp.o: \
- /home/gator/vmmul-omp-harness-instructional/dgemv-vectorized.cpp \
- /usr/include/stdc-predef.h
