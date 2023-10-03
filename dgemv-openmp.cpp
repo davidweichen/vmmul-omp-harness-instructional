b639b36c17d39a6132fed0d5c70106b20078748d
davidweichen <david19990212@gmail.com>
2023-10-02 22:06:57 -0700

    parallel

--- dgemv-openmp.cpp
+++ dgemv-openmp.cpp
@@ -16,7 +16,7 @@ void my_dgemv(int n, double* A, double* x, double* y) {
 
 
 
-   #pragma omp parallel shared(A,x)
+   #pragma omp parallel
    {
       int nthreads = omp_get_num_threads();
       int thread_id = omp_get_thread_num();
